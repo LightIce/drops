6e18201304cb5e76f39425f8f3c7824ea5a77063
lightless <lightless@foxmail.com>
2014-08-01 20:56:20 +0800

    add md5.js and finish md5 function

---
+++ js/system.js
@@ -0,0 +1,11 @@
+// submit the form and md5 the password and confirm password
+function SubmitForm() {
+	document.getElementById("password").value = hex_md5(document.getElementById("password").value);
+	document.getElementById("ConfirmPassword").value = hex_md5(document.getElementById("ConfirmPassword").value);
+	this.signup.submit();
+}
+
+function SubmitLoginForm() {
+	document.getElementById("password").value = hex_md5(document.getElementById("password").value);
+	this.login.submit();
+}
